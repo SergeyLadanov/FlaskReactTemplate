2b802bbbf8500dfc85bec7b68fd40dce291bff51
Sergey Ladanov <sergei_ladanov_93@mail.ru>
2024-09-16 10:25:16 +0300

    Добавлены примеры

--- react/src/pages/About.tsx
+++ react/src/pages/About.tsx
@@ -1,19 +1,66 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import '../scss/styles.scss';
+import $ from 'jquery'; // Импортируем jQuery
 
 
 function About()
 {
-    return (
+    const [val1, setVal1] = useState('');
+    const [val2, setVal2] = useState('');
+    const [loading, setLoading] = useState(true);
 
-        <div className="row">
-            <div className="col-md-12">
-                <p><h3>About page jhbjh</h3></p>
-            </div>
-            <a href="#" className="btn btn-secondary" role = "button">Click me</a>
-        </div>
 
-    );
+    useEffect(() => {
+        
+           // Выполняем запрос на сервер при монтировании компонента
+           $.ajax({
+            url: './get_data',
+            method: 'GET',
+            dataType: 'json',
+            success: (response) => {
+                setVal1(response['param1']);
+                setVal2(response['param2']);
+            },
+            error: (jqXHR, textStatus, errorThrown) => {
+              //setError(`Ошибка: ${textStatus}, ${errorThrown}`);
+            },
+            complete: () => {
+              setLoading(false);
+            }
+          });
+
+
+        return () => {
+            // код выполняется при размонтировании компонента (закрытии)
+            console.log('Component unmounted');
+          };
+    });
+
+    if (loading)
+    {
+            return (
+                <div>Загрузка данных</div>
+            );
+    }
+    else
+    {
+    return ( 
+                <div className="row">
+                    <div className="col-md-12">
+                        <p><h3>{val1}</h3></p>
+                    </div>
+
+                    <div className="col-md-12">
+                        <p><h3>{val2}</h3></p>
+                    </div>
+
+                    <div className="col-md-12">
+                        <p><h3>About page jhbjh</h3></p>
+                    </div>
+                    <a href="#" className="btn btn-secondary" role = "button">Click me</a>
+                </div>
+            );
+    }
 }
 
 export default About;
--- react/src/pages/Main.tsx
+++ react/src/pages/Main.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import '../scss/styles.scss';
 
 
@@ -8,12 +8,38 @@ function Main()
 {
     let refreshIntervalId:NodeJS.Timeout;
 
+    const [val1, setVal1] = useState('');
+    const [val2, setVal2] = useState('');
+    const [loading, setLoading] = useState(true);
+
     useEffect(() => {
         
         refreshIntervalId = setInterval(()=>{
             console.log("Main...")
         }, 1000);
 
+
+           // Выполняем запрос на сервер при монтировании компонента
+            const fetchData = async () => {
+                try {
+                const response = await fetch('./get_data');
+                if (!response.ok) {
+                    throw new Error('Ошибка при выполнении запроса');
+                }
+                const result = await response.json();
+                setVal1(result['param1']);
+                setVal2(result['param2']);
+                } catch (error) {
+                //setError(error.message);
+                } finally {
+                setLoading(false);
+                }
+            };
+
+            fetchData();
+
+
+        
         return () => {
             // код выполняется при размонтировании компонента (закрытии)
             console.log('Component unmounted');
@@ -21,25 +47,41 @@ function Main()
           };
     });
 
+    if (loading)
+        {
+            return (
+                <div>Загрузка данных</div>
+            );
+        }
+    else
+    {
+        return (
+                <div className="row">
+                    {/* <div className="row" style={{textAlign: "center"}} id = "loading">
+                        <p>
+                        <div className="col-md-12">
+                            <div className="spinner-border" role="status">
+                            <span className="visually-hidden">Loading...</span>
+                            </div>
+                        </div>
+                        </p>
+                    </div> */}
 
-    return (
-        <div className="row">
-            {/* <div className="row" style={{textAlign: "center"}} id = "loading">
-                <p>
-                <div className="col-md-12">
-                    <div className="spinner-border" role="status">
-                    <span className="visually-hidden">Loading...</span>
+                    <div className="col-md-12">
+                        <p><h3>Start page</h3></p>
                     </div>
-                </div>
-                </p>
-            </div> */}
 
-            <div className="col-md-12">
-                <p><h3>Start page</h3></p>
-            </div>
-            <a href="#" className="btn btn-primary" role = "button">Click me</a>
-        </div>
-    );
+                    <div className="col-md-12">
+                        <p><h3>{val1}</h3></p>
+                    </div>
+
+                    <div className="col-md-12">
+                        <p><h3>{val2}</h3></p>
+                    </div>
+                    <a href="#" className="btn btn-primary" role = "button">Click me</a>
+                </div>
+            );
+    }
 }
 
 export default Main;
